5669d11c0ec4b6a9766931bcc44c9596e8be7189
nixgank-wang <kylewang0529@gmail.com>
2020-09-18 15:48:31 -0400

    passed test

--- R/linear_model.R
+++ R/linear_model.R
@@ -1,7 +1,7 @@
 
 #' @title  Linear Model
 #' @name linear_model
-#' @description This is a function takes a formula and a dataset as input, 
+#' @description This is a function takes a formula and a dataset as input,
 #' fit a linear model based on the input and returns a list of coefficients
 #' @param form A given formula for fitting the regression.
 #' @param data A given dataset to fit the model.
@@ -19,8 +19,10 @@ linear_model = function(form, data,contrasts = NULL){
   X <- model.matrix(form, data_no_na,contrasts.arg = contrasts)
   y_name<- as.character(form)[2]
   Y <- matrix(data_no_na[,y_name],ncol=1)
-  beta<- solve(t(X)%*%X) %*% t(X) %*% Y
-  
+  #beta<- solve(t(X)%*%X) %*% t(X) %*% Y
+  beta<-solve.qr(qr(X),Y)
+  beta[which(beta==0)]=NA
+
   beta_names <- rownames(beta)
   beta <-as.numeric(beta)
   names(beta) <- beta_names
@@ -28,7 +30,7 @@ linear_model = function(form, data,contrasts = NULL){
   class(ret) <- "my_lm"
   ret
 }
-#reorganize the output 
+#reorganize the output
 print.my_lm <- function(x,...){
   cat("\nCoefficients:\n",x$coefficients)
 }
---
+++ R/lm_patho-doc.R
@@ -0,0 +1,11 @@
+
+#' Data from the IMAP study
+#'
+#' @name lm_patho
+#' @docType data
+#' @rdname lm_patho
+#' @description
+#' This dataset is a test set for linear_model function
+#' @references  Refer to the first homework description of BIS557
+#' @keywords data
+NULL

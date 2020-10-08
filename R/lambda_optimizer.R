9becdf06ccb469320487d00e22f6e59f3e8de153
nixgank-wang <kylewang0529@gmail.com>
2020-10-08 05:41:15 -0400

    a

--- R/lambda_optimizer.R
+++ R/lambda_optimizer.R
@@ -1,5 +1,16 @@
+#' @title Optimizing ridge parameter
+#' @description This function optimizes the ridge parameter by comparing the mean value of residuals
+#' @param form A formula
+#' @param data The dataset that we use to fit the model
+#' @param v    Number of folds in cross-validation
+#' @param lambdas A vector of lambdas
+#' @importFrom foreach %do% foreach
+#' @importFrom tibble tibble
+#' @importFrom rsample vfold_cv
 
-
+#' @examples
+#'lambda_optimizer(form=form,data=iris,v=10,lambdas=lambdas)
+#' @export
 lambda_optimizer<-function(form,data,v,lambdas){
   #create v folds for cross validation
   folds <-vfold_cv(data,v=v)
@@ -10,7 +21,7 @@ lambda_optimizer<-function(form,data,v,lambdas){
         as.vector(assessment(fold)[,y_name] - predict(fit, assessment(fold)))
       }
    }
-  
+
   rd <- tibble(lambda=lambdas, meam = map_dbl(resids,mean))
   rd
 }

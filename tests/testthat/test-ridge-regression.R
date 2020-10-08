93e43b687dbc73f4d0f4689f0a5b51d62feec272
nixgank-wang <kylewang0529@gmail.com>
2020-10-08 03:48:16 -0400

    update

--- tests/testthat/test-ridge-regression.R
+++ tests/testthat/test-ridge-regression.R
@@ -14,9 +14,6 @@ test_that("Your ridge_regression() function works well", {
   fit_lm.ridge= lm.ridge(Y ~ ., data=test_data, lambda = 0.01)
   #implemt our function
   fit_ridge_regression = lm_ridge(Y ~ .,data=test_data , lambda = 0.01)
-
-  expect_equivalent(unlist(fit_lmridge$coef), fit_ridge$coefficients,
-                    tolerance = 0.05)
   expect_equivalent(coef(fit_lm.ridge), fit_ridge_regression$coefficients,
                     tolerance = 0.1)
 })
